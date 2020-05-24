d945d5049391063508b528aa6ceb97cf23d75b45
Choieunji0114 <cheunji8209@naver.com>
2020-05-25 03:08:55 +0900

    Upload musicplayer, connectAPI

---
+++ connect-api/connect-api/APIService/AqiService.swift
@@ -0,0 +1,17 @@
+//
+//  AqiService.swift
+//  connect-api
+//
+//  Created by 최은지 on 02/05/2020.
+//  Copyright © 2020 최은지. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AqiService {
+    
+    static let shared = AqiService()
+    
+   
+}
